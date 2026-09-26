b56171b2dcffbda49817702e40ea9907f77bd395
mdmashka <91541905+mdmashka@users.noreply.github.com>
2026-09-26 03:01:00 +0300

    feat: add native iOS shell

---
+++ ios/GymTracker/GymTrackerApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea(.container, edges: .bottom)
+        }
+    }
+}
